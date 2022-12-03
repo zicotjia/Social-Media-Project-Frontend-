77cb5b7cd925f9eeccbcd5ed42c9184d106b82fa
zicot <zicotjia@gmail.com>
2022-12-03 16:31:35 +0800

    Fix bug with myPost page not filtering post

--- src/pages/MyPost.tsx
+++ src/pages/MyPost.tsx
@@ -1,18 +1,23 @@
-import * as React from 'react';
-import { Box, useColorModeValue, VStack } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { Post } from '../models/Post';
-import { useAppDispatch, useAppSelector } from '../hooks/redux/hooks';
-import { getFile } from '../hooks/redux/actions/postActions';
-import ImagePost from '../components/Post/ImagePost';
-import { User } from '../models/User';
+import * as React from "react";
+import { Box, useColorModeValue, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Post } from "../models/Post";
+import { useAppDispatch, useAppSelector } from "../hooks/redux/hooks";
+import { getFile } from "../hooks/redux/actions/postActions";
+import ImagePost from "../components/Post/ImagePost";
+import { User } from "../models/User";
 
 function MyPost() {
-  const { user }: { user: User } = useAppSelector((state) => state.currUserReducer);
-  const { posts }: { posts: Post[] } = useAppSelector((state) => state.postsReducer);
+  const { user }: { user: User } = useAppSelector(
+    (state) => state.currUserReducer
+  );
+  const { posts }: { posts: Post[] } = useAppSelector(
+    (state) => state.postsReducer
+  );
 
   var copy = posts;
-  copy = copy.filter((post) => (post.user._id = user._id));
+
+  copy = copy.filter((post) => post.user._id === user._id);
 
   const dispatch = useAppDispatch();
 
@@ -22,7 +27,12 @@ function MyPost() {
 
   return (
     <>
-      <Box ml={{ base: 0, md: 60 }} minH="100vh" bg={useColorModeValue('blue.100', 'grey.900')} pt={2}>
+      <Box
+        ml={{ base: 0, md: 60 }}
+        minH="100vh"
+        bg={useColorModeValue("blue.100", "grey.900")}
+        pt={2}
+      >
         <VStack align="center" spacing={2}>
           {copy &&
             copy.map((post, index) => {
